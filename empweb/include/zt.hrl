41edc3d635529b97b0e6bae3a07a70863429d468
w-495 <w@w-495.ru>
2012-08-01 20:38:10 +0400

    evennts

---
+++ empweb/include/zt.hrl
@@ -0,0 +1,9 @@
+-ifndef(__EMPWEB_47479714120__).
+-define(__EMPWEB_47479714120__, true).
+
+-define(FUNCTION, '__function_macro__').
+-define(ARITY, '__function_arity__').
+
+-endif. %%% __EMPWEB_47479714120__
+
+
