d1981c00b5131d77f240054bf1df4600973357ca
w-495 <w@w-495.ru>
2012-08-01 20:38:04 +0400

    evennts

---
+++ empweb/src/zt.erl
@@ -0,0 +1,25 @@
+-module(zt).
+
+-export([parse_transform/2]).
+
+-include("zt.hrl").
+
+parse_transform(AST, _Options) -> [parse(T) || T <- AST].
+
+parse({function, _, FName, FArity, _} = T) -> erl_syntax_lib:map(fun(TE)
+    -> parsemacro(FName, FArity, TE) end, T);
+parse(T) -> T.
+
+parsemacro(FName, FArity, T) ->
+    erl_syntax:revert(
+        case erl_syntax:type(T) of
+            atom ->
+                case erl_syntax:atom_value(T) of
+                    ?FUNCTION -> erl_syntax:atom(FName);
+                    ?ARITY -> erl_syntax:integer(FArity);
+                    _ -> T
+                end;
+            _ ->
+                T
+        end
+    ).  
