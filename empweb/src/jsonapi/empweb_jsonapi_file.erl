2ec5207591bc986f9887e3b736fe315ced7ecf0d
w-495 <w@w-495.ru>
2012-11-13 18:27:00 +0400

    ++

---
+++ empweb/src/jsonapi/empweb_jsonapi_file.erl
@@ -0,0 +1,110 @@
+%%
+%% @file    empweb_jsonapi_pers.erl
+%%          "Контроллер" для функций работы с системными настройками,
+%%          языками, и связанными с ними объектами.
+%%
+
+-module(empweb_jsonapi_file).
+-behavior(empweb_http_hap).
+
+%% ===========================================================================
+%% Заголовочные файлы
+%% ===========================================================================
+
+%%
+%% Определения общие для всего приложения
+%%
+-include("empweb.hrl").
+
+%%
+%% Описание структур нормировки полей
+%%
+-include_lib("norm/include/norm.hrl").
+
+%%
+%% Описание записей событий и макросов
+%%
+-include_lib("evman/include/events.hrl").
+
+
+%%
+%% Трансформация для получения имени функции.
+%%
+-include_lib("evman/include/evman_transform.hrl").
+
+
+%% ===========================================================================
+%% Экспортируемые функции
+%% ===========================================================================
+
+-export([
+    init/3,
+    handle/2,
+    terminate/2
+]).
+
+%% ===========================================================================
+%% Внешние функции
+%% ===========================================================================
+
+%%
+%% @doc Инициализация запроса
+%%
+init(_, Req, #empweb_hap{
+        action          =   Action,
+        params          =   Params,
+        is_auth         =   Is_auth,
+        pers_id         =   Pid,
+        pers_perm_names =   Pns
+    } = Hap)->
+    %%%
+    %%% Это нужно, чтобы понять, какая функция дальше выполнится
+    %%%
+    ?evman_notice({hap, [
+        {action,            Action},
+        {params,            Params},
+        {is_auth,           Is_auth},
+        {pers_id,           Pid},
+        {pers_perm_names,   Pns}
+    ]}, <<" = Hap">>),
+
+    {ok,
+        Req,
+        #empweb_hap{
+            action          =   Action,
+            params          =   Params,
+            is_auth         =   Is_auth,
+            pers_id         =   Pid,
+            pers_perm_names =   Pns
+        }
+    }.
+
+handle(_req, #empweb_hap{
+        action=create, params=Params, pers_id=Pers_id, is_auth=true
+    } = Hap) ->
+    ?evman_args([Hap], <<" = create file">>),
+    {ok,
+        empweb_jsonapi:resp(
+            empweb_biz_file:create([
+                {owner_id, Pers_id}|Params
+            ])
+        ),
+        Hap
+    };
+
+handle(_req, #empweb_hap{
+        action=Action, params=Params, is_auth=Is_auth, pers_id=Pers_id
+    } = Hap) ->
+    ?evman_notice({hap, [
+        {forbidden,     true},
+        {action,        Action},
+        {params,        Params},
+        {pers_id,       Pers_id},
+        {is_auth,       Is_auth}
+    ]}, <<" = forbidden">>),
+    {ok,empweb_jsonapi:forbidden(), Hap}.
+
+
+terminate(_req, Hap)->
+    ?evman_args([Hap], <<" = terminate">>),
+    ok.
