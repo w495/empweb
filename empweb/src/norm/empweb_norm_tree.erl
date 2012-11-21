69448591491ec870f8756d7e879f3908e17bae84
w-495 <w@w-495.ru>
2012-11-21 13:15:58 +0400

    geo

---
+++ empweb/src/norm/empweb_norm_tree.erl
@@ -0,0 +1,86 @@
+-module(empweb_norm_tree).
+
+-include("empweb.hrl").
+
+-include_lib("norm/include/norm.hrl").
+
+%%
+%% Описание записей событий и макросов
+%%
+-include_lib("evman/include/events.hrl").
+
+
+%%
+%% Трансформация для получения имени функции.
+%%
+-include_lib("evman/include/evman_transform.hrl").
+
+
+-export([
+    norm/1
+]).
+
+
+norm('get') ->
+    [
+        #norm_rule{
+            key         = parent_id,
+            required    = false,
+            types       = [integer]
+        },
+        #norm_rule{
+            key         = nchildtargets,
+            required    = false,
+            types       = [integer]
+        },
+        #norm_rule{
+            key         = nnodetargets,
+            required    = false,
+            types       = [integer]
+        },
+        #norm_rule{
+            key         = nchildren,
+            required    = false,
+            types       = [integer]
+        },
+        #norm_rule{
+            key         = nnodes,
+            required    = false,
+            types       = [integer]
+        }
+        |empweb_norm_opt:norm('get')
+    ];
+
+norm('create') ->
+    [
+        #norm_rule{
+            key         = parent_id,
+            required    = false,
+            types       = [integer]
+        }
+        |empweb_norm_opt:norm('create')
+    ];
+
+norm('update') ->
+    [
+        #norm_rule{
+            key         = parent_id,
+            required    = false,
+            types       = [integer]
+        }
+        |empweb_norm_opt:norm('update')
+    ];
+
+norm('delete') ->
+    [
+        #norm_rule{
+            key         = parent_id,
+            required    = false,
+            types       = [integer]
+        }
+        |empweb_norm_opt:norm('delete')
+    ];
+
+norm(X) ->
+    empweb_norm_opt:norm(X).
+
