da517e962ebab21679254963e7cacf1a026b9dc3
w-495 <w@w-495.ru>
2012-11-26 13:09:47 +0400

    album path

---
+++ empweb/src/jsonapi/empweb_jsonapi_transtype.erl
@@ -0,0 +1,200 @@
+%%
+%% @file    empweb_jsonapi_pers.erl
+%%          "Контроллер" для функций работы с системными настройками,
+%%          языками, и связанными с ними объектами.
+%%
+
+-module(empweb_jsonapi_transtype).
+-behavior(empweb_http_hap).
+
+%% ===========================================================================
+%% Заголовочные файлы
+%% ===========================================================================
+
+%%
+%% Определения общие для всего приложения
+%%
+-include("empweb.hrl").
+
+%%
+%% Описание структур нормировки полей
+%%
+-include_lib("norm/include/norm.hrl").
+
+%%
+%% Описание записей событий и макросов
+%%
+-include_lib("evman/include/events.hrl").
+
+
+%%
+%% Трансформация для получения имени функции.
+%%
+-include_lib("evman/include/evman_transform.hrl").
+
+
+%% ===========================================================================
+%% Экспортируемые функции
+%% ===========================================================================
+
+-export([
+    init/3,
+    handle/2,
+    terminate/2
+]).
+
+%% ===========================================================================
+%% Внешние функции
+%% ===========================================================================
+
+%%
+%% @doc Инициализация запроса
+%%
+init(_, Req, #empweb_hap{
+        action          =   Action,
+        params          =   Params,
+        is_auth         =   Is_auth,
+        pers_id         =   Pid,
+        pers_perm_names =   Pns
+    } = Hap)->
+    %%%
+    %%% Это нужно, чтобы понять, какая функция дальше выполнится
+    %%%
+    ?evman_notice({hap, [
+        {action,            Action},
+        {params,            Params},
+        {is_auth,           Is_auth},
+        {pers_id,           Pid},
+        {pers_perm_names,   Pns}
+    ]}, <<" = Hap">>),
+
+    {ok,
+        Req,
+        #empweb_hap{
+            action          =   Action,
+            params          =   Params,
+            is_auth         =   Is_auth,
+            pers_id         =   Pid,
+            pers_perm_names =   Pns
+        }
+    }.
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Языки
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+%         id,
+%         roomlot_id,
+%         room_id,
+%         room_head,
+%         owner_id,
+%         owner_nick
+% 
+
+
+        
+handle(_req, #empweb_hap{
+        action='get', params=Params, pers_id=Pers_id
+    } = Hap) ->
+    ?evman_args([Hap], <<" = get transtype">>),
+
+    empweb_jsonapi:handle_params(
+        %% проверка входных параметров и приведение к нужному типу
+        norm:norm(Params, [
+            #norm_rule{
+                key         = isincome,
+                required    = false,
+                types       = [nullable, boolean]
+            }
+            |empweb_norm_opt:norm('get')
+        ]),
+        fun(Data)->
+            ?evman_debug(Data, <<" = Data">>),
+            {ok,
+                empweb_jsonapi:resp(
+                    empweb_biz_transtype:get(
+                        empweb_norm:filter_owner([
+                            {pers_id, Pers_id}
+                            |Data#norm.return
+                        ]),
+                        proplists:get_value(fields, Data#norm.return, [])
+                    )
+                ),
+                Hap
+            }
+        end
+    );
+
+handle(_req, #empweb_hap{
+        action=create, params=Params, pers_id=Pers_id
+    } = Hap) ->
+    ?evman_args([Hap], <<" = create transtype">>),
+    empweb_jsonapi:handle_params(
+        %% проверка входных параметров и приведение к нужному типу
+        norm:norm(Params, [
+            #norm_rule{
+                key         = isincome,
+                required    = false,
+                types       = [nullable, boolean]
+            }
+            |empweb_norm_opt:norm('create')
+        ]),
+        fun(Data)->
+            ?evman_debug(Data, <<" = Data">>),
+            {ok,
+                empweb_jsonapi:resp(
+                    empweb_biz_transtype:create([
+                        {owner_id, Pers_id}
+                        |Data#norm.return
+                    ])
+                ),
+                Hap
+            }
+        end
+    );
+
+handle(_req, #empweb_hap{
+        action=update, params=Params, pers_id=Pers_id
+    } = Hap) ->
+    ?evman_args([Hap], <<" = update transtype">>),
+
+    empweb_jsonapi:handle_params(
+        %% проверка входных параметров и приведение к нужному типу
+        norm:norm(Params, [
+            #norm_rule{
+                key         = isincome,
+                required    = false,
+                types       = [nullable, boolean]
+            }
+            |empweb_norm_opt:norm('update')
+        ]),
+        fun(Data)->
+            ?evman_debug(Data, <<" = Data">>),
+            {ok,
+                empweb_jsonapi:resp(
+                    empweb_biz_transtype:update([
+                        {owner_id, Pers_id}
+                        |Data#norm.return
+                    ])
+                ),
+                Hap
+            }
+        end
+    );
+
+handle(_req, #empweb_hap{
+        action=Action, params=Params, is_auth=Is_auth, pers_id=Pers_id
+    } = Hap) ->
+    ?evman_notice({hap, [
+        {forbidden,     true},
+        {action,        Action},
+        {params,        Params},
+        {pers_id,       Pers_id},
+        {is_auth,       Is_auth}
+    ]}, <<" = forbidden">>),
+    {ok,empweb_jsonapi:forbidden(), Hap}.
+
+
+terminate(_req, Hap)->
+    ?evman_args([Hap], <<" = terminate">>),
+    ok.
