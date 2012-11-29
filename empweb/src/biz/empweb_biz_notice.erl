2f7cc251dca8f6b8268179c553bf8673355cc3ae
w-495 <w@w-495.ru>
2012-11-29 14:15:09 +0400

    notice

---
+++ empweb/src/biz/empweb_biz_notice.erl
@@ -0,0 +1,57 @@
+%%
+%% @file    empweb_biz_conf.erl
+%%          Бизнес логика по работе с системными настройками,
+%%          языками, и связанными с ними объектами.
+%%
+%%          Зависит от модуля empdb_biz_notice. Все внешние функуции принимают
+%%              proplist()
+%%          и возвращают:
+%%              {ok, [Obj::{proplist()}]}
+%%              |   {error, Reason::atom()}
+%%              |   {error, {Reason::atom(), Info::atom()}}
+%%              |   {error, {Reason::atom(), Info::[Obj::{proplist()}]}}
+%%
+-module(empweb_biz_notice).
+
+%% ==========================================================================
+%% Экспортируемые функции
+%% ==========================================================================
+
+%%
+%% Списки языков
+%%
+-export([
+    update/1,
+    create/1,
+    get/1,
+    get/2,
+    delete/1
+]).
+
+
+%% ==========================================================================
+%% Внешние функции
+%% ==========================================================================
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Списки языков
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+create(Params)->
+    empdb_biz_notice:create(Params).
+
+update(Params)->
+    empdb_biz_notice:update(Params).
+
+get(Params)->
+    empdb_biz_notice:get([{isdeleted, false}|Params]).
+
+get(Params, Fields)->
+    empdb_biz_notice:get([{isdeleted, false}|Params], Fields).
+
+delete(Params)->
+    empdb_biz_notice:update([
+        {filter, [{isdeleted, false}|Params]},
+        {values, [{isdeleted, true}]}
+    ]).
+
