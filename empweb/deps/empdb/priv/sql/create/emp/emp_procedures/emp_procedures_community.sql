cd1c0dfbf9762c3377485c30f6f4f4ea50948511
w-495 <w@w-495.ru>
2012-12-06 19:35:53 +0400

    ++

--- empweb/deps/empdb/priv/sql/create/emp/emp_procedures/emp_procedures_community.sql
+++ empweb/deps/empdb/priv/sql/create/emp/emp_procedures/emp_procedures_community.sql
@@ -44,11 +44,6 @@ begin
     /**
     *  Типы сообщества
     **/
-    update pers set
-        live_community_approved = true
-    where pers.id = (select owner_id from doc where doc.id = new.doc_id);
-    
-    
     new.nmembs = 1;
     if (not (new.communitytype_id is null))
         and (new.communitytype_alias is null) then
@@ -76,6 +71,25 @@ $$ language plpgsql;
 drop trigger if exists t1community_util_fields_on_insert on community ;
 create trigger t1community_util_fields_on_insert before insert
 on community for each row execute procedure community_util_fields_on_insert();
+
+
+create or replace function community_util_fields_after_insert() returns "trigger" as $$
+begin
+    /**
+    *  Типы сообщества
+    **/
+    update pers set
+        live_community_approved = true,
+        live_community_id = new.doc_id
+    where pers.id = (select owner_id from doc where doc.id = new.doc_id);
+    return new;
+end;
+$$ language plpgsql;
+
+
+drop trigger if exists t1community_util_fields_after_insert on community ;
+create trigger t1community_util_fields_after_insert after insert
+on community for each row execute procedure community_util_fields_after_insert();
 
 
 
