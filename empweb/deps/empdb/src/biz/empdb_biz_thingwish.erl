b88a4dc549e6a56b2f28cba221b034141cab5dd7
w-495 <w@w-495.ru>
2012-12-17 14:03:01 +0400

    ++

---
+++ empweb/deps/empdb/src/biz/empdb_biz_thingwish.erl
@@ -0,0 +1,76 @@
+%% Author: w-495
+%% Created: 25.07.2012
+%% Description: TODO: Add description to biz_user
+-module(empdb_biz_thingwish).
+
+%% ===========================================================================
+%% Заголовочные файлы
+%% ===========================================================================
+
+%%
+%% Структры для работы с запросами к базе данных
+%%
+-include("empdb.hrl").
+
+
+%% ==========================================================================
+%% Экспортируемые функции
+%% ==========================================================================
+
+%%
+%% Блоги
+%%
+-export([
+    get/1,
+    get/2,
+    create/1,
+    update/1
+]).
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%%                          ЗНАЧИМЫЕ ОБЪЕКТЫ
+%%
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Покупки
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+create(Params)->
+    empdb_dao:with_transaction(fun(Con)->
+        empdb_dao_thingwish:create(Con, Params)
+    end).
+
+update(Params)->
+    empdb_dao:with_transaction(fun(Con)->
+        empdb_dao_thingwish:update(Con, Params)
+    end).
+
+delete(Params)->
+    empdb_dao:with_transaction(fun(Con)->
+        empdb_dao_thingwish:delete(Con, [
+            {fields, [
+                {isdeleted, false}
+                |Params
+            ]},
+            {values, [
+                {isdeleted, true}
+            ]}
+        ])
+    end).
+
+get(Params)->
+    empdb_dao:with_transaction(fun(Con)->
+        empdb_dao_thingwish:get(Con, [{isdeleted, false}|Params])
+    end).
+
+get(Params, Fileds)->
+    empdb_dao:with_transaction(fun(Con)->
+        empdb_dao_thingwish:get(Con, [{isdeleted, false}|Params], Fileds)
+    end).
+
+is_owner(Uid, Oid)->
+    empdb_dao:with_transaction(fun(Con)->
+        empdb_dao_thingwish:is_owner(Con, Uid, Oid)
+    end).
