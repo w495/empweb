6a351bab2385c06da33d12fc25d12403f7963ffd
nikitin.i <w@w-495.ru>
2013-05-16 23:54:32 +0400

    fix thingbuy and dao null

--- empweb/deps/empdb/src/daowp/empdb_daowp_room.erl
+++ empweb/deps/empdb/src/daowp/empdb_daowp_room.erl
@@ -55,34 +55,48 @@ get_adds(Con, {ok, Rooms}, Params) ->
                     ]),
 
 
-                %empdb_dao_thingbuy:get(emp, [
-                    %{room_id, 10622},
-                    %{file_id, 1071},
-                    %{limit, 1},
-                    %{fields, [file_id]}
-                %]).
+                %Room_id = proplists:get_value(id, Roompl, null),
+                %Old_back_file_id =
+                    %proplists:get_value(back_file_id, Roompl)
+
+                %Nroompl =
+                    %case empdb_dao_thingbuy:get(Con, [
+                        %{room_id, Room_id},
+                        %{file_id, proplists:get_value(back_file_id, Roompl)},
+                        %{limit, 1},
+                        %{fields, [file_id]}
+                    %]) of
+                        %{ok, []} ->
+                            %Default_room_background =
+                                %empdb_dao_file:get(Con, [{alias, default_room_background}]),
+                            %New_back_file_id =
+                                %proplists:get_value(id, Default_room_background),
+                            %case Old_back_file_id of
+                                %New_back_file_id ->
+                                    %Roompl;
+                                %_ ->
+                                    %{ok, _} =
+                                        %empdb_dao_room:update(emp, [
+                                            %{filter, [
+                                                %{id, Room_id},
+                                                %{back_file_id, {'neq', New_back_file_id}}
+                                            %]}
+                                            %{values, [
+                                                %{back_file_id, New_back_file_id}
+                                            %]}
+                                        %]),
+                                    %[
+                                        %{back_file_id, New_back_file_id}
+                                        %|proplists:delete(back_file_id, Roompl)
+                                    %]
+                            %end;
+                        %_ ->
+                            %Roompl
+                    %end,
 
 
-                Nroompl =
-                    case empdb_dao_thingbuy:get(Con, [
-                        {room_id, proplists:get_value(id, Roompl)},
-                        {file_id, proplists:get_value(back_file_id, Roompl)},
-                        {limit, 1},
-                        {fields, [file_id]}
-                    ]) of
-                        {ok, []} ->
-                            Default_room_background = empdb_dao_file:get(Con, [{alias, default_room_background}]),
-                            [
-                                {back_file_id, proplists:get_value(id, Default_room_background)}
-                                |proplists:delete(back_file_id, Roompl)
-                            ];
-                        _ ->
-                            Roompl
-                    end,
 
-
-
-                Backfilepath = filepath(Con, Nroompl, back_file_id, default_room_background),
+                Backfilepath = filepath(Con, Nroompl, back_file_id),
                 Wallfilepath = filepath(Con, Nroompl, wall_file_id),
                 Flagfilepath = filepath(Con, Nroompl, flag_file_id),
                 Armsfilepath = filepath(Con, Nroompl, arms_file_id),
@@ -126,7 +140,7 @@ filepath(Con, Roompl, Idfield, Alias) ->
         {empdb_dao_fileinfo, file_id}
     ],Con,[
         {'or', [
-            {'file.id', proplists:get_value(Idfield, Roompl)},
+            {'file.id', proplists:get_value(Idfield, Roompl, null)},
             {'and', [
                 {'file.alias', {'neq', null}},
                 {'file.alias', Alias}
